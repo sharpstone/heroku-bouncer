d51e58bd7049eb5e98c376b2014e393746180249
Raul Murciano <raul@murciano.net>
2013-12-07 20:42:34 -0800

    Tests for `expose_email`

---
+++ test/expose_email.rb
@@ -0,0 +1,59 @@
+require_relative "test_helper"
+
+describe Heroku::Bouncer do
+  include Rack::Test::Methods
+
+  context "expose_email: true" do
+    before do
+      @app = app_with_bouncer do
+        {
+          oauth: { id: '46307a2b-0397-4739-b2b7-2f67d1cff597', secret: '46307a2b-0397-4739-b2b7-2f67d1cff597' },
+          secret: ENV['HEROKU_BOUNCER_SECRET'],
+          expose_email: true
+        }
+      end
+    end
+
+    context "after a successful OAuth dance" do
+      before do
+        get '/hi'
+        follow_successful_oauth!
+      end
+
+      it "exposes the email" do
+        assert_redirected_to_path('/hi')
+        follow_redirect!
+
+        assert last_request.env['bouncer.email']
+        assert_equal 'hi', last_response.body
+      end
+    end
+  end
+
+  context "expose_email: false" do
+    before do
+      @app = app_with_bouncer do
+        {
+          oauth: { id: '46307a2b-0397-4739-b2b7-2f67d1cff597', secret: '46307a2b-0397-4739-b2b7-2f67d1cff597' },
+          secret: ENV['HEROKU_BOUNCER_SECRET'],
+          expose_email: false
+        }
+      end
+    end
+
+    context "after a successful OAuth dance" do
+      before do
+        get '/hi'
+        follow_successful_oauth!
+      end
+
+      it "does not expose the email" do
+        assert_redirected_to_path('/hi')
+        follow_redirect!
+
+        assert last_request.env['bouncer.email'].nil?
+        assert_equal 'hi', last_response.body
+      end
+    end
+  end
+end
