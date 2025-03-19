7d3b8adf7c6413f3bc3e5fb8e049625cd68ca423
Chris Montes <cmontes@heroku.com>
2025-03-19 13:52:55 -0500

    Revert "Use built-in cookie decryption (#74)"
    This reverts commit 99357858cb25801541ea611d11ceed46619519a7.

--- test/skip_test.rb
+++ test/skip_test.rb
@@ -31,10 +31,8 @@ describe Heroku::Bouncer do
     private
 
     def decode_cookie(raw_cookie)
-      @encryptor ||= Rack::Session::Encryptor.new(default_bouncer_config.fetch(:secret))
-      @encryptor.decrypt(raw_cookie)
-    rescue Rack::Session::Encryptor::InvalidSignature
-      {}
+      unescaped_cookie = URI::Parser.new.unescape(raw_cookie.split("\n").join)
+      Marshal.load(Base64.decode64(unescaped_cookie.split("--").first))
     end
   end
 end
